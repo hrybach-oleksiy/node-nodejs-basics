1cb2a4ed1057e76ac136c014b101d4493dcf4e4a
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-10-07 17:33:24 +0300

    feat: implement stream task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.resolve(__dirname, 'files', 'fileToRead.txt');
+
+  const fileStream = createReadStream(filePath);
+
+  fileStream.pipe(process.stdout);
+
+  fileStream.on('error', (err) => {
+    console.error('FS operation failed', err);
+  });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import { createWriteStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.resolve(__dirname, 'files', 'fileToWrite.txt');
+
+  const writeStream = createWriteStream(filePath);
+
+  process.stdin.pipe(writeStream);
+
+  writeStream.on('finish', () => {
+    console.log('Data has been written to file.');
+  });
+
+  writeStream.on('error', (err) => {
+    console.error('FS operation failed', err);
+  });
 };
 
 await write();
