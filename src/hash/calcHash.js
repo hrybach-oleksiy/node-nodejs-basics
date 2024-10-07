72b1bfb63c1fb26fb801d334594321d8d063c102
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-10-07 16:56:14 +0300

    feat: implement Hash Task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const filePath = path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+  const hash = createHash('sha256');
+  const fileStream = createReadStream(filePath);
+
+  fileStream.on('data', (chunk) => {
+    hash.update(chunk); 
+  });
+
+  fileStream.on('end', () => {
+    const result = hash.digest('hex'); 
+    console.log(result); 
+  });
+
+  fileStream.on('error', (err) => {
+    console.error('FS operation failed', err);
+  });
 };
 
 await calculateHash();
